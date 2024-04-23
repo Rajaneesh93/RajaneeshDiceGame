ef8f58c843bb836e099b2a08031e65b84d6e53a2
Rajaneesh93 <52157559+Rajaneesh93@users.noreply.github.com>
2024-04-23 19:25:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImg = "dice"+randomNumber1+".png";
+
+var Dicesrc = "./images/"+randomDiceImg;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",Dicesrc)
+
+//Dice2
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImg2 = "dice"+randomNumber2+".png";
+
+var Dicesrc2 = "./images/"+randomDiceImg2;
+
+var image2 = document.querySelectorAll("img")[1]
+
+image2.setAttribute("src",Dicesrc2)
+
+//Dice3
+
+var randomNumber3 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImg3 = "dice"+randomNumber3+".png";
+
+var Dicesrc3 = "./images/"+randomDiceImg3;
+
+var image3 = document.querySelectorAll("img")[2]
+
+image3.setAttribute("src",Dicesrc3)
+
+//Dice4
+
+var randomNumber4 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImg4 = "dice"+randomNumber4+".png";
+
+var Dicesrc4 = "./images/"+randomDiceImg4;
+
+var image4 = document.querySelectorAll("img")[3]
+
+image4.setAttribute("src",Dicesrc4)
+
+//Dice5
+
+var randomNumber5 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImg5 = "dice"+randomNumber5+".png";
+
+var Dicesrc5 = "./images/"+randomDiceImg5;
+
+var image5 = document.querySelectorAll("img")[4]
+
+image5.setAttribute("src",Dicesrc5)
+
+//Dice5
+
+var randomNumber6 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImg6 = "dice"+randomNumber6+".png";
+
+var Dicesrc6 = "./images/"+randomDiceImg6;
+
+var image6 = document.querySelectorAll("img")[5]
+
+image6.setAttribute("src",Dicesrc6)
+
